bd9809496d789c1cb422d164185d5f1799ff5f77
Qurtis6 <122908229+Qurtis6@users.noreply.github.com>
2025-01-31 21:02:02 -0500

    Mobile version attempt 4, I think it works now

--- nav.js
+++ nav.js
@@ -26,4 +26,39 @@ var html =
 </a>\
 </div>';
 
-document.getElementById("nav").innerHTML = html;
+var hamburger =
+  '<h1>Bunk House News</h1>\
+  <a href="index.html">\
+  <img src="media/bunkhouse-logo.png" class="logo" />\
+</a>\
+    <div class="off-screen-menu">\
+      <ul>\
+        <li><a href="meet.html">Meet the <br >Bunk House</a></li>\
+        <li><a href="history.html">History</a></li>\
+        <li><a href="shop.html">Shop</a></li>\
+        <li><a href="games.html">Games</a></li>\
+        <li><a href="master-kirby.html">Master<br >Kirby</a></li>\
+      </ul>\
+    </div>\
+    <nav>\
+      <div class="ham-menu">\
+        <span></span>\
+        <span></span>\
+        <span></span>\
+      </div>\
+    </nav>';
+
+const mediaQuery = window.matchMedia("(max-width: 768px)");
+if (mediaQuery.matches) {
+  document.getElementById("nav").innerHTML = hamburger;
+  const hamMenu = document.querySelector(".ham-menu");
+
+  const offScreenMenu = document.querySelector(".off-screen-menu");
+
+  hamMenu.addEventListener("click", () => {
+    hamMenu.classList.toggle("active");
+    offScreenMenu.classList.toggle("active");
+  });
+} else {
+  document.getElementById("nav").innerHTML = html;
+}
